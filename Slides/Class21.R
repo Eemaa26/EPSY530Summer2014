ef7b6bdd5bc1059328199be6cb57b9302da35b13
Jason Bryer <jason@bryer.org>
2014-06-18 18:34:14 -0400

    Fixed slides.

--- Slides/Class21.R
+++ Slides/Class21.R
@@ -1,5 +1,5 @@
 
-
+## ----, echo=FALSE, results='hide', message=FALSE, warning=FALSE----------
 require(ggplot2)
 require(gdata)
 require(psych)
@@ -8,19 +8,20 @@ opts_chunk$set(fig.path='Figures/Class21-')
 opts_chunk$set(comment=NA)
 
 require(granova)
+require(granovaGG)
 data(anorexia.sub)
 
 
-
+## ----, echo=FALSE--------------------------------------------------------
 print(anorexia.sub, row.names=FALSE)
 
 
-
+## ----, echo=FALSE--------------------------------------------------------
 anorexia.sub$Diff <- anorexia.sub[,2] - anorexia.sub[,1]
 print(anorexia.sub, row.names=FALSE)
 
 
-
+## ----, granovads,fig.show='hide', fig.width=10, fig.height=9-------------
 granovagg.ds(anorexia.sub[,1:2], revc=TRUE)
 
 
